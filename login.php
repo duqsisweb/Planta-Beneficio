40dac6e148f8ca1fed3616e8c5f75e2e8294a206
duqsisweb <desarrolladorweb@duquesa.com.co>
2023-03-27 10:48:21 -0500

    Diseño Login

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php header('Content-Type: text/html; charset=UTF-8');
-    error_reporting(0);?>
+error_reporting(0); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,13 +10,11 @@
     <meta name="description" content="Plataforma Planta Beneficio" />
     <meta name="author" content="Yon Gonzalez" />
     <title>Portal empleados</title>
-    <link rel="icon" type="image/x-icon" href="../assets/image/faviconplanta.png" />
+    <link rel="icon" type="image/x-icon" href="./assets/image/faviconplanta.png" />
     <link rel="stylesheet" href="./css/bootstrap.css">
 
-<!-- Fuentes -->
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <!-- Fuentes -->
+
 
 </head>
 
@@ -26,21 +24,21 @@
             <div class="row">
                 <div class="col-md-4"></div>
                 <div class="col-md-4 " id="cajaloginformulario">
-                <div class="cssFont_1" style="font-family:'Montserrat', sans-serif">Planta Beneficio</div>
+                    <div class="cssFont_1" style="font-family:'Montserrat', sans-serif">Planta Beneficio</div>
                     <div id="logopalmerasform">
                         <img src="./assets/image/palmerasdelllanologoform.png" alt="Palmeras" href="">
                     </div>
                     <form autocomplete="off" action="aute.php" method="POST">
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label" id="textoformulario">Usuario</label>
-                            <input type="text" class="form-control" id="" aria-describedby="" name="usuario"  autocomplete="off" required>
-                            <div id="textoformulario" class="form-text">We'll never share your email with anyone else.</div>
+                            <input type="text" class="form-control" id="" aria-describedby="" name="usuario" autocomplete="off" required>
+                            <div id="textoformulario" class="form-text"></div>
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label" id="textoformulario">Contraseña</label>
-                            <input type="password" class="form-control" id="textoformulario" name="password"  autocomplete="off" required>
+                            <input type="password" class="form-control" id="textoformulario" name="password" autocomplete="off" required>
                         </div>
-                        <button type="submit" class="btn btn-success" name="iniciar" value="Ingresar" >Iniciar</button>
+                        <button type="submit" class="btn btn-success" name="iniciar" value="Ingresar">Iniciar</button>
                     </form>
                 </div>
                 <div class="col-md-4"></div>
@@ -48,7 +46,23 @@
         </div>
     </section>
 
+    <footer>
+        <div class="container-fluid" id="footerloginGlobal">
+            <div class="row">
+                <div class="col-md-6" id="footerlogin">
+                    <img src="./assets/image/palmerasdelllanofooter1.png" alt=" Palmeras" href="" style="width: 100%;">
+                </div>
+                <div class="col-md-6" id="footerlogin2">
+                    <div>
+                        <p id="textofooter">| Sistemas | Correo: desarrolladorweb@duquesa.com. ©2023 All Rights Reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
 </body>
+
 </html>
 
 
